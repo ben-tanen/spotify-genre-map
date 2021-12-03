38c9c31d656c940222264cea761302267c8a5cc9
ben-tanen <ben.tanen@gmail.com>
2021-12-02 21:27:19 -0700

    adding preview data + tweaking workflow

--- scripts/scrape-enao-genres.py
+++ scripts/scrape-enao-genres.py
@@ -22,12 +22,20 @@ driver.get(url)
 # scrape all genres + convert style attributes to DataFrame
 genres_elems = driver.find_elements_by_class_name("genre")
 
-genres_objs = [{
-    style.split(":")[0].strip().replace("-", "_"): style.split(":")[1].strip() 
-    for style in ("genre: %s;%s" % (genre.get_attribute("innerText"), genre.get_attribute("style"))).split(";") 
-    if len(style) > 0
- } for genre in genres_elems]
-
+genres_objs = [ ]
+for genre in genres_elems[:10]:
+    genre_obj = {
+        "genre": genre.get_attribute("innerText"),
+        "preview_url": genre.get_attribute("preview_url"),
+        "preview_track": genre.get_attribute("title")
+    }
+    
+    for style in genre.get_attribute("style").split(";")[:-1]:
+        [key,value] = style.split(":")
+        genre_obj[key.strip().replace("-", "_")] = value.strip()
+    
+    genres_objs.append(genre_obj)
+    
 genres_df = pd.DataFrame(genres_objs)
 
 # add run date to df
